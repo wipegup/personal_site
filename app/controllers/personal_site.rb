3898fb257902e4a09920285272ccf28d2d79a40c
wipegup <wipegup@gmail.com>
2019-03-02 07:53:40 -0700

    Add call method

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,3 +1,7 @@
-class PersonalSite
+require 'rack'
 
+class PersonalSite
+  def self.call(env)
+    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+  end
 end
