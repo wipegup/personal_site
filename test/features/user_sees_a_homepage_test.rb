83f714c6e569feac2db56ef54d9a52e53f43d43a
wipegup <wipegup@gmail.com>
2019-03-02 08:00:15 -0700

    Add default save path for capybara

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -3,7 +3,7 @@ require './test/test_helper'
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
-
+    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
