6f481dc1dc3bde59ae826def87528927fd4034a1
wipegup <wipegup@gmail.com>
2019-03-09 09:03:20 -0700

    Adjust personal site to point to html

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,6 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['Welcome!']] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
   end
 end
