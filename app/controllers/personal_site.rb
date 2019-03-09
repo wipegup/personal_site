bae8e8d4a046a699edfc7d37ca6a42b0e717153c
wipegup <wipegup@gmail.com>
2019-03-09 09:04:33 -0700

    Add instructions for error page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -2,6 +2,18 @@ require 'rack'
 
 class PersonalSite
   def self.call(env)
-    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]] # Recall, this array includes the HTTP response status code, HTTP response headers & HTTP body
+    case env["PATH_INFO"]
+    when '/' then index
+    else
+      error
+    end
+  end
+
+  def self.index
+    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+  end
+
+  def self.error
+    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
   end
 end
