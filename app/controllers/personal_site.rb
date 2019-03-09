2fa5bd2067720b1bd2df26a03814894f55e8f854
wipegup <wipegup@gmail.com>
2019-03-09 09:06:16 -0700

    Add logic for about page

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,16 +4,25 @@ class PersonalSite
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
+    when '/about' then about
     else
       error
     end
   end
 
   def self.index
-    ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
   end
 
   def self.error
-    ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
+    render_view('error.html', '404')
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
   end
 end
